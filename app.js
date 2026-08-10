457bc9a23378a24669ad4e4966d9ee9c688d42fc
Mauriv10 <mauri.villalobos92@gmail.com>
2026-08-10 10:51:06 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APP_VERSION=globalThis.WC26_CONFIG?.version||"704.11.15";
+const APP_VERSION=globalThis.WC26_CONFIG?.version||"704.11.16";
 const DATA_SCHEMA_VERSION=2;
 const DATA_REVISION="2026-07-17-collections-v70111";
 const MASTER_SEED_KEY="world-cup-2026-master-seed-revision";
