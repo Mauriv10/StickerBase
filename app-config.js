d493663863a9b1b34ceb3830f9b68d33856b7e4f
Mauriv10 <mauri.villalobos92@gmail.com>
2026-08-10 15:54:41 +0200

    Add files via upload

--- app-config.js
+++ app-config.js
@@ -1,6 +1,6 @@
 globalThis.WC26_CONFIG = {
-  version: "704.11.17",
-  cacheName: "wc26-build-704-11-17",
+  version: "704.12.1",
+  cacheName: "wc26-build-704-12-1",
   versionUrl: "./version.json",
   updateCheckIntervalMs: 15 * 60 * 1000
 };
